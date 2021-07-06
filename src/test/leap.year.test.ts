568f28c1077a2497c3db7a72f9fad583d0c2fd9e
Hudson Steffani Soares Rosa <hudsonssrosa@gmail.com>
2021-07-06 21:28:42 +0100

    Remove comments

--- src/test/leap.year.test.ts
+++ src/test/leap.year.test.ts
@@ -14,34 +14,4 @@ describe('.isLeapYear(year)', () => {
         expect(isLeapYear(year)).toBe(expected);
     });
 
-    // it("should return false if not divisible by 4", () => {
-    //     let leapYear = isLeapYear(2001);
-    //     expect(leapYear).toBe(false);
-    // })
-
-    // it("should return true if is divisible by 4", () => {
-    //     let leapYear = isLeapYear(2016);
-    //     expect(leapYear).toBe(true);
-    // })
-
-    // it("should return true if its divisible by 400", () => {
-    //     let leapYear = isLeapYear(1600);
-    //     expect(leapYear).toBe(true);
-    // })
-
-    // it("should return false if its divisible by 100", () => {
-    //     let leapYear = isLeapYear(1800);
-    //     expect(leapYear).toBe(false);
-    // })
-
-    // it("should return true if its divisible by 0", () => {
-    //     let leapYear = isLeapYear(0);
-    //     expect(leapYear).toBe(true);
-    // })
-
-    // it("should return true if its divisible by -400", () => {
-    //     let leapYear = isLeapYear(-400);
-    //     expect(leapYear).toBe(true);
-    // })
-
 })
